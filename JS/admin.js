8d8218210e20439af34fd2fb08c9cd302f37803f
tyhieshajohnson <johnsontyhiesha@gmail.com>
2023-12-02 12:06:43 +0200

    javaScript

--- JS/admin.js
+++ JS/admin.js
@@ -2,16 +2,17 @@
 // variable
 let items = []
 // all products need to be saved in the admin page and it will be iterated to other pages
+// DON'T USE THIS FUNCTION
 let object1 = {
-    id: 1,
-    name: 'Nike Sneaker',
-    description: "Airforce 1",
-    price: 100,
-    url: 'https://i.postimg.cc/65Kg26W8/sneakers.png',
+    // id: 1,
+    name: 'Travis Scott',
+    description: "Air Jordan 1 High",
+    price: 3500,
+    url: 'https://i.postimg.cc/Lhd8hK1M/Best-Travis-Scott-Nike-Sneakers-10.webp',
 }
 
-// contructor function
-function constructor (id, name, description, price, url) {
+// MUST USE A contructor function to create the objects(use PascalCase)
+function Constructor (id, name, description, price, url) {
     this.id = id;
     this.name = name;
     this.description = description;
@@ -20,7 +21,7 @@ function constructor (id, name, description, price, url) {
 }
 
 // creating the object
-let item2 = new constructor(2, "Airforce 1", 200, 'https://i.postimg.cc/65Kg26W8/sneakers.png')
+let item2 = new Constructor(2, "Travis Scott", "Air Jordan 1 Low", 4300, 'https://i.postimg.cc/FzLykv7r/R-2.jpg')
 
 // pushing intems into an array storing
 items.push(object1, item2); 
@@ -43,8 +44,35 @@ let products = items.map(function(item, index) {
     console.log(index);
     return `
     <tr>
+    <td>${index+1}</td>
     <td>${item.name}</td>
+    <td>${item.price}</td>
+    <td>${item.description}</td>
+    <td><image src='${item.url}'</td>
+    <td><button>Edit</button></td>
+    <td><button class="delete" value='${index}'>Delete</button></td>
 </tr
 `
 })
 table.innerHTML = products;
+
+// how to style in JavaScript
+// table.style.display = 'none';
+// table.style.textAlign = 'center';
+table.style.backgroundColor = 'grey';
+table.style.textDecorationColor = 'white';
+
+// 
+let deleteButton = document.querySelector('.delete');
+function remove(position){
+    // alert('Button Clicked1')
+    items.splice(position, 1);
+};
+table.addEventListener('click', function(){
+    // using a conditional statement by declaring the button that is clicked
+    if (event.target.classList.contains('delete')){
+        // remove()
+        // alert('event.targer.value)');
+        remove(event.target.value)
+    }
+})
